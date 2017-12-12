8a0695114771826a5668da268bdd3a31a94f0562
Tsao <J30081@NGC.AUTHORITY>
2017-12-11 21:11:38 -0500

    Did some code cleaning and added some comments. Tried a couple different kernel sizes

--- src/main.cu
+++ src/main.cu
@@ -14,24 +14,36 @@
 #include <opencv2/core/opengl_interop.hpp>
 
 #define THREADS_PER_WARP 32
-#define BLOCK_SIZE 32
+#define BLOCK_SIZE 16
+__constant__ float MIN_STD = 0.1f;
 
 template<class T>
 __device__ void changeStatistics(const cv::gpu::PtrStepSz<T> src,
 									cv::gpu::PtrStepSz<float> dst,
 									const float *means, 
 									const float *stds, 
-									const float sigma)
+									const float sigma,
+									const int tid_x,
+									const int tid_y,
+									const int x,
+									const int y,
+									const int rows,
+									const int cols)
 {
-	const int tid_x = threadIdx.x;
-	const int tid_y = threadIdx.y;
-	const int x = (blockIdx.x * blockDim.x) + tid_x;
-	const int y = (blockIdx.y * blockDim.y) + tid_y;
-	const int rows = src.rows;
-	const int cols = src.cols;
-
+	
+	// Set the desired value of the pixel based of the statistics of the region. Also handle edge cases where the standard deviation is close to zero
 	if (y < rows && x < cols) {
-		dst.ptr(y)[x] = (((float(src.ptr(y)[x]) - means[0])/stds[0]) * (sigma + stds[0])) + means[0];
+		float sigma_target;
+		// If the standard deviation is too low, don't enhance the noise
+		if (stds[0] < MIN_STD) {
+			sigma_target = 1.f;
+		}
+		else {
+			sigma_target = sigma / stds[0];
+		}
+
+		dst.ptr(y)[x] = (float(src.ptr(y)[x]) - means[0]) * sigma_target + means[0];
+
 		// Handle cases for when image is larger than bounds
 		if (dst.ptr(y)[x] > 255)
 			dst.ptr(y)[x] = 255.f;
@@ -42,37 +54,22 @@ __device__ void changeStatistics(const cv::gpu::PtrStepSz<T> src,
 }
 
 template<class T>
-__device__ void minusMeanSquared(const cv::gpu::PtrStepSz<T> src, cv::gpu::PtrStepSz<float> dst, float *means)
-{
-	const int tid_x = threadIdx.x;
-	const int tid_y = threadIdx.y;
-	const int x = (blockIdx.x * blockDim.x) + tid_x;
-	const int y = (blockIdx.y * blockDim.y) + tid_y;
-	const int rows = src.rows;
-	const int cols = src.cols;
-
-	if (y < rows && x < cols) {
-		dst.ptr(y)[x] = powf((float(src.ptr(y)[x]) - means[0]), 2.f);
-	}
-	__syncthreads();
-}
-
-template<class T>
-__device__ void calcBlockMean(const cv::gpu::PtrStepSz<T> src, float *dst) {
+__device__ void calcBlockMean(const cv::gpu::PtrStepSz<T> src,
+								float *dst,
+								const int tid_x,
+								const int tid_y,
+								const int x,
+								const int y,
+								const int block_size,
+								const int block_num,
+								const int rows,
+								const int cols) {
 	/*
-	Calculate the mean per block
+	Calculate the mean per block efficently 
 	*/
-	const int tid_x = threadIdx.x;
-	const int tid_y = threadIdx.y;
 	const int x_bound = (blockDim.x / 2);
-	const int block_size = blockDim.x*blockDim.y;
-	const int x = (blockIdx.x * blockDim.x) + tid_x;
-	const int y = (blockIdx.y * blockDim.y) + tid_y;
 	const int block_ind = (x_bound * threadIdx.y) + tid_x;
-	const int block_num = (blockIdx.y * gridDim.x) + blockIdx.x;
 	const int tid = (tid_y * blockDim.x) + tid_x;
-	const int rows = src.rows;
-	const int cols = src.cols;
 
 	// Load global data into shared memory while completing first add for speed
 	//		and make sure we are within image boundaries and block boundaries
@@ -100,28 +97,41 @@ __global__ void lace(const cv::gpu::PtrStepSz<unsigned char> src,
 					cv::gpu::PtrStepSz<float> dst,
 					const double sigma)
 {
+	// Declare constants to be used in kernel
 	const int tid_x = threadIdx.x;
 	const int tid_y = threadIdx.y;
 	const int tid = (tid_y * blockDim.x) + tid_x;
 	const int block_num = (blockIdx.y * gridDim.x) + blockIdx.x;
+	const int x = (blockIdx.x * blockDim.x) + tid_x;
+	const int y = (blockIdx.y * blockDim.y) + tid_y;
+	const int block_size = blockDim.x*blockDim.y;
+	const int rows = src.rows;
+	const int cols = src.cols;
+	
 
 	// Create shared memory for faster calculations
-	__shared__ float means[BLOCK_SIZE*BLOCK_SIZE / 2];
-	__shared__ float stds[BLOCK_SIZE*BLOCK_SIZE / 2];
+	__shared__ float means_shared[BLOCK_SIZE*BLOCK_SIZE / 2];
+	__shared__ float stds_shared[BLOCK_SIZE*BLOCK_SIZE / 2];
 
 	// Calculate mean per block
-	calcBlockMean<unsigned char>(src, means);
+	calcBlockMean<unsigned char>(src, means_shared, tid_x, tid_y, x, y, block_size, block_num, rows, cols);
 
 	// Calculate standard deviation per block
-	minusMeanSquared(src, dst, means);
-	calcBlockMean<float>(dst, stds);
+	// Subtract mean from pixel and square it for standard deviation calculation
+	if (y < rows && x < cols) {
+		dst.ptr(y)[x] = powf((float(src.ptr(y)[x]) - means_shared[0]), 2.f);
+	}
+	__syncthreads();
+	// Average the squared values
+	calcBlockMean<float>(dst, stds_shared, tid_x, tid_y, x, y, block_size, block_num, rows, cols);
+	// Complete standard deviation calculation
 	if (tid == 0) {
-		stds[0] = sqrt(stds[0]);
+		stds_shared[0] = sqrt(stds_shared[0]);
 	}
 	__syncthreads();
 
 	// Adjust statistics of image locally
-	changeStatistics<unsigned char>(src, dst, means, stds, sigma);
+	changeStatistics<unsigned char>(src, dst, means_shared, stds_shared, sigma, tid_x, tid_y, x, y, rows, cols);
 }
 
 void local_contrast_enhancement(cv::gpu::GpuMat& src,
@@ -144,6 +154,7 @@ void local_contrast_enhancement(cv::gpu::GpuMat& src,
 
 	// Allocate room for destination matrix
 	cv::gpu::GpuMat laced(rows, cols, CV_32FC1);
+	cv::gpu::GpuMat means(rows, cols, CV_32FC1);
 
 	// Call LACE Kernel
 	dim3 blockDim(BLOCK_SIZE, BLOCK_SIZE);
@@ -179,7 +190,7 @@ int main()
 	cv::gpu::GpuMat laced;
 	
 	// Apply contrast enhancement
-	local_contrast_enhancement(gpu_image, laced, 6.5f);
+	local_contrast_enhancement(gpu_image, laced, 8.0f);
 
 	// Send data back from GPU to host
 	cv::Mat laced_host;
